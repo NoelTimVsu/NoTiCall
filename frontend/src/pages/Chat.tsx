21b1cf423080259281caa7f5eed13265d79f510b
Noel Rojas <noelrojas@noelrojas.local>
2025-05-07 11:13:45 -0400

    Noel-14::getting rid of double scrolling on CHAT page

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -267,8 +267,8 @@ function Chat() {
   const { selectedUser } = useChatStore();
 
   return (
-    <div className="h-screen">
-      <div className="flex items-center justify-center pt-20 px-4">
+    <div className="h-[calc(100%-70px)]">
+      <div className="flex items-center justify-center pt-5 px-4">
         <div className="bg-white rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
             <Sidebar />
